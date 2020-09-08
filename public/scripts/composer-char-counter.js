88f8a770d207fc6955f53ffe72df19f4411ac61a
cplpearce <clint.pearce@rocketmail.com>
2020-09-08 14:42:54 -0400

    added header hexagon, and textarea script

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
   // on key-up, update the new tweet counter to length
   $( "#tweet-text" ).on('keyup', function() {
-    $( ".counter" ).html(`${140 - this.innerHTML.length}`);
-    if (140 - this.innerHTML.length < 0) {
+    $( ".counter" ).html(`${140 - this.value.length}`);
+    if (140 - this.value.length < 0) {
       $( ".counter" ).css('color', 'red');
     } else {
       $( ".counter" ).css('color', '#545149');
--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -1,14 +1,17 @@
 $( document ).ready(function() {
+  /* increase tweet-text height dynmaically */
+  autosize($( "#tweet-text" ));
   /* fade out the tweet-text label on input */
   $( "#tweet-text" ).focus(function() {
     $( "#tweet-text-label" ).fadeTo( 2500, 0, function() {
     });
   });
   /* fade in the tweet-text label if the field is empty */
-  $( "#tweet-text" ).on('keyup', function() {
+  $( "#tweet-text" ).on('blur', function() {
     if (this.innerHTML.length === 0) {
-      $( "#tweet-text-label" ).fadeTo( 2500, 1, function() {
+      $( "#tweet-text-label" ).fadeTo( 2500, .7, function() {
       });
     }
   });
+
 });
