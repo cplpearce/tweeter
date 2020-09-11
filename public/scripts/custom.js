cf2507b1a30e9474fed763d82be5de544a51a80f
cplpearce <clint.pearce@rocketmail.com>
2020-09-10 21:04:52 -0400

    scroll to top btn, slight css nav tweaks

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -1,38 +1,27 @@
 // a message toast for the user
 const toast = (msg, success = false) => {
-  const thisToast = new Promise((res, err) => {
+  const thisToast = new Promise(() => {
     // generate toast uid, though I imagine there are
-    // more concise, npm ways this is simple and low cost
+    // more concise, npm-ish ways this is simple and low cost
     const uid = Math.random().toString(36).substr(2, 5);
     // ternary on success, default to false, create toast
     success
     ? $( 'body ').append(`<div id="toast" class="${uid} success"></div>`)
     : $( 'body ').append(`<div id="toast" class="${uid} fail"></div>`);
     // jquery selector on div #id toast
-    const toast = $( `#toast.${uid}` );
+    const toastID = $( `#toast.${uid}` );
     // set the div html to [msg], then fadeIn .5 sec delay 2 sec, fadeOut 1 sec
-    toast.html(msg) && toast.fadeTo(500, 1).delay(500).fadeTo(1000, 0, () => {
+    toastID.html(msg) && toastID.fadeTo(500, 1).delay(500).fadeTo(1000, 0, () => {
       // burn the toast
       $( `#toast.${uid}` ).remove();
     });
   });
 }
-
-// animate something up and down by 10px
-const wiggle = (selector) => {
-  selector
-    .animate({'top': '+=10',}, 500, () => {
-      selector.animate({'top': '-=10',}, 500)
-    });
-};
-
 // listeners and ready functions
 $( document ).ready(() => {
-  // give the form-toggle a shake so the user knows its there
-  wiggle($( '#nav-arrow' ));
   // increase tweet-text height dynmaically
   autosize($( "#tweet-text" ));
-  // fade out the tweet-text label on input
+  // fade out the tweet-text label on input (css can't easily affect DOM elements above)
   $( "#tweet-text" ).focus(function() {
     $( "#tweet-text-label" ).fadeTo(1500, 0, function() {
     })
@@ -44,17 +33,25 @@ $( document ).ready(() => {
       });
     }
   });
-  // wiggle $( '#nav-arrow' ) on mouseover of $( '#nav-form-toggle' )
-  $( '#nav-form-toggle' ).hover(() => {
-    wiggle($( '#nav-arrow' ));
-  }, () => {
-  $( '#nav-arrow' ).finish;
-  });
   // flip arrow and unhide new tweet
-  $( '#nav-form-toggle' ).click(() => {
-    $( '#new-tweet' ).css('display') === 'none'
-    ? $( '#nav-form-toggle' ).animate({ 'fontSize': '-=10', }, 400)
-    : $( '#nav-form-toggle' ).animate({'fontSize': '+=10' }, 400);
-    $( '#new-tweet' ).slideToggle(300, 'linear') && $( '#nav-arrow' ).toggleClass('flip');
+  $( '#nav-form-toggle' ).click((event) => {
+    $( '#nav-form-toggle' ).toggleClass('navClicked');
+    $( '#new-tweet' ).slideToggle(500, 'linear');
+    $( '#tweet-text' ).focus()
   });
+  // fade in out scroll button at 150px
+  $(document).scroll(function() {
+    if ($(this).scrollTop() > 150) {
+      console.log('test')
+      $( '#scroll-up' ).fadeIn(700);
+    } else {
+      $( '#scroll-up' ).fadeOut(700);
+    }
+  });
+  // scroll to the top of our page when $( '#scroll-up' ) is clicked
+  $( '#scroll-up' ).click(() => {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $( 'head' ).offset().top
+    }, 300);
+});
 });
